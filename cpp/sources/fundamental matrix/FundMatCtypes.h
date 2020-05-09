e9ac14f2b6d05c2d6d526fe7f02847f6bb6b401a
yx87cn <65076485+yx87cn@users.noreply.github.com>
2020-05-09 23:15:48 +0800

    Add files via upload

---
+++ cpp/sources/fundamental matrix/FundMatCtypes.h
@@ -0,0 +1,34 @@
+/*	FundMatCtypes.h
+This is a wrapper file to 
+convert output in Ctypes for Python.
+
+x1, y1, x2, y2 : 2D image pairs
+inputNum : total amount of inputs
+trial : number of trials.
+*/
+
+#ifndef FUNDMATCTYPES
+#define FUNDMATCTYPES
+
+extern "C"
+{
+	struct Structure // return type of structure
+	{
+		double StructureStrength;		// strength
+		size_t StructureSize;			// size		
+		double StructureScale;			// scale
+		size_t *StructureIndex;			// Indices
+		double *StructureTLS;			// TLS estimate
+	};
+
+	_declspec(dllexport)
+		Structure *
+		FundMatCtypes(const double *x1, const double *y1, 
+						const double *x2, const double *y2,
+						const size_t inputNum, const size_t trial);
+
+	_declspec(dllexport)
+		void FreeMemory(Structure *);
+}
+
+#endif
