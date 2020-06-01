3dba456c50bc74c4d1c472d3a1423d6246172452
Josep Ramon Morros <ramon.morros@upc.edu>
2020-06-02 00:03:13 +0200

    library exported functions now compatible with Linux/MacOS

--- cpp/sources/ellipse/EllipseCtypes.h
+++ cpp/sources/ellipse/EllipseCtypes.h
@@ -10,6 +10,33 @@ trial : number of trials.
 #ifndef ELLIPSECTYPES
 #define ELLIPSECTYPES
 
+#include <stddef.h>
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef BUILDING_DLL
+    #ifdef __GNUC__
+#define DLL_PUBLIC __attribute__ ((dllexport))
+    #else
+#define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+#define DLL_PUBLIC __attribute__ ((dllimport))
+    #else
+#define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
+  #define DLL_LOCAL
+#else
+  #if __GNUC__ >= 4
+#define DLL_PUBLIC __attribute__ ((visibility ("default")))
+#define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+  #else
+    #define DLL_PUBLIC
+    #define DLL_LOCAL
+  #endif
+#endif
+
 extern "C"
 {
 	struct Structure // return type of structure
@@ -21,12 +48,14 @@ extern "C"
 		double *StructureTLS;			// TLS estimate
 	};
 
-	_declspec(dllexport)
+        //_declspec(dllexport)
+        DLL_PUBLIC
 		Structure *
 		EllipseCtypes(const double *x, const double *y,
 						const size_t inputNum, const size_t trial);
 
-	_declspec(dllexport)
+        //_declspec(dllexport)
+	DLL_PUBLIC
 		void FreeMemory(Structure *);
 }
 
